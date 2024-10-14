2376034ceb06a9bf196acc44f644ac65bce8ec4a
IbMoh <115637313+IbMoh@users.noreply.github.com>
2024-10-14 21:39:16 +0200

    Started the creation of the LandingPage (dummy data inside)

--- app/src/main/java/com/example/tryggakampus/presentation/landingPage/LandingPage.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/landingPage/LandingPage.kt
@@ -1,36 +1,61 @@
 package com.example.tryggakampus.presentation.landingPage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.tryggakampus.presentation.component.PageContainer
+
 
 @Composable
 fun LandingPage(title: String) {
     PageContainer {
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-        Button(onClick = {}) { Text("Click Here") }
-        Button(onClick = {}) { Text("Click Here") }
-        Text("this is a very long sentence on my page, how will this appear on mobile screen")
-
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+                    .background(Color.White)
+                    .fillMaxWidth(),
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+            Row {
+                Text(
+                    text = "title",
+                    modifier = Modifier,
+                    color = Color.hsl(
+                        hue = 330f,
+                        saturation = 0.75f,
+                        lightness = 0.7f
+                    ),
+                )
+                Spacer(
+                    modifier = Modifier
+                        .padding(16.dp)
+                )
+                Text(
+                    text = "Lorem epsom",
+                    modifier = Modifier,
+                    color = Color.White,
+                )
+            }
+        }
     }
 }
