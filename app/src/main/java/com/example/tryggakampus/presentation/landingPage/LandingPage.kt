1d6447d9e7f90aa3031ea85d10d134b275cbfbb1
IbMoh <115637313+IbMoh@users.noreply.github.com>
2024-10-15 12:42:44 +0200

    Fixed the landing page, added button functionality

--- app/src/main/java/com/example/tryggakampus/presentation/landingPage/LandingPage.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/landingPage/LandingPage.kt
@@ -1,59 +1,147 @@
 package com.example.tryggakampus.presentation.landingPage
-
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tryggakampus.LocalNavController
+import com.example.tryggakampus.Routes
+
 import com.example.tryggakampus.presentation.component.PageContainer
 
 
 @Composable
 fun LandingPage(title: String) {
-    PageContainer {
+
+    val viewModel: LandingPageModelView = viewModel()
+    val navController = LocalNavController.current
+
+    val gradientColors = listOf(
+        Color.Cyan,
+        Color.hsl(hue = 210f, saturation = 0.6f, lightness = 0.8f),
+        Color.hsl(hue = 330f, saturation = 0.7f, lightness = 0.7f))
+
+    PageContainer(
+        modifier = Modifier
+            .background(Color.hsl(169f, 1f,0.93f))
+    ) {
         Column(
             modifier = Modifier
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = title,
-                modifier = Modifier
-                    .padding(bottom = 16.dp)
-                    .background(Color.White)
-                    .fillMaxWidth(),
-                color = Color.Black,
-                textAlign = TextAlign.Center
-            )
-            Row {
-                Text(
-                    text = "title",
-                    modifier = Modifier,
-                    color = Color.hsl(
-                        hue = 330f,
-                        saturation = 0.75f,
-                        lightness = 0.7f
-                    ),
+            Row{
+                Image(
+                    painter = painterResource(viewModel.image),
+                    contentDescription = "Logo"
                 )
                 Spacer(
                     modifier = Modifier
-                        .padding(16.dp)
+                        .padding(10.dp)
                 )
                 Text(
-                    text = "Lorem epsom",
-                    modifier = Modifier,
+                    text = viewModel.title,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 30.dp),
+                    color = Color.Black,
+                    textAlign = TextAlign.Center,
+                    fontSize = 30.sp,
+                    style = TextStyle(
+                        brush = Brush.linearGradient(
+                            colors = gradientColors
+                        )
+                    ),
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Cursive
+                )
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .padding(50.dp)
+            )
+
+            Text(
+                text = viewModel.aboutUs,
+                modifier = Modifier,
+                color = Color.Black,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.W700
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(9.dp)
+            )
+            Text(
+                text = viewModel.info1,
+                modifier = Modifier,
+                color = Color.Black,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.W300
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(36.dp)
+                )
+            Text(
+                text = viewModel.button,
+                modifier = Modifier,
+                color = Color.Black,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.W700,
+                textAlign = TextAlign.Left
+            )
+            Spacer(
+                modifier = Modifier
+                    .padding(9.dp)
+            )
+            Text(
+                text = viewModel.buttonInfo,
+                modifier = Modifier,
+                color = Color.Black,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.W300,
+                textAlign = TextAlign.Center
+            )
+            Button(
+                onClick = {
+
+                    navController.navigate(Routes.ProfilePage())
+                },
+                modifier = Modifier
+                    .padding(top = 26.dp)
+                    ,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Red
+                )
+            ) {
+                Text(
+                    text = "Get in touch!",
                     color = Color.White,
+                    fontWeight = FontWeight.Bold
                 )
             }
         }
--- app/src/main/java/com/example/tryggakampus/presentation/landingPage/LandingPageModelView.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/landingPage/LandingPageModelView.kt
@@ -1,4 +1,18 @@
 package com.example.tryggakampus.presentation.landingPage
 
-class LandingPageModelView {
+import androidx.lifecycle.ViewModel
+import com.example.tryggakampus.R
+
+class LandingPageModelView: ViewModel() {
+
+    val title: String = "Welcome to Trygga Kampus!"
+    val aboutUs: String = "About Us!"
+    val info1: String = "Here will be info regarding the app like what we aim to do and how it functions"
+    val button: String = "" +
+            "If you need serous help, please Get in touch with either you University counselor or one of our " +
+            "professionals using this button."
+    val buttonInfo: String = "This button will prompt you with a forum that you fill in with what you're dealing with" +
+            " and send it to someone to provide the help needed."
+    val image = R.drawable.logo_2
+
 }
