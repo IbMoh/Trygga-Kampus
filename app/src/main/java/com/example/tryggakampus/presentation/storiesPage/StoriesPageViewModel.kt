906e9e76fbf1cdcc38122a3441a04cd87543d304
idontreallywolf <nedim.kanat.lp@gmail.com>
2024-10-16 22:33:37 +0200

    clear viewmodel list to avoid duplication

--- app/src/main/java/com/example/tryggakampus/presentation/storiesPage/StoriesPageViewModel.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/storiesPage/StoriesPageViewModel.kt
@@ -14,6 +14,7 @@ class StoriesPageViewModel: ViewModel() {
         private set
 
     suspend fun loadStories() {
+        stories.clear()
         stories.addAll(StoryRepositoryImpl().getAllStories())
         Log.d("LOAD", "stories: ${stories.size}")
     }
