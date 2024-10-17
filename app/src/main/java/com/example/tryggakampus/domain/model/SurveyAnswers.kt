20bac2f7ad15626610c1aae3bcae395cd8926da0
Ibrahim Mohamed <115637313+IbMoh@users.noreply.github.com>
2024-10-17 19:29:54 +0200

    Update app/src/main/java/com/example/tryggakampus/domain/model/SurveyAnswers.kt
    Co-authored-by: Nedkan <nedim.kanat.lp@gmail.com>

--- app/src/main/java/com/example/tryggakampus/domain/model/SurveyAnswers.kt
+++ app/src/main/java/com/example/tryggakampus/domain/model/SurveyAnswers.kt
@@ -1,5 +1,8 @@
 package com.example.tryggakampus.domain.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class SurveyAnswer(
     val question: String,
     val answer: String
