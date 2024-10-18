9ecc8fce65f653e1bab3764951ce169313056c6b
idontreallywolf <nedim.kanat.lp@gmail.com>
2024-10-18 02:49:53 +0200

    refactor repository to a singleton object

--- app/src/main/java/com/example/tryggakampus/domain/repository/StoryRepository.kt
+++ app/src/main/java/com/example/tryggakampus/domain/repository/StoryRepository.kt
@@ -2,22 +2,27 @@ package com.example.tryggakampus.domain.repository
 
 import android.util.Log
 import com.example.tryggakampus.domain.model.StoryModel
+import com.google.firebase.firestore.Source
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.tasks.await
 
 interface StoryRepository {
-    suspend fun getAllStories(): List<StoryModel>
+    suspend fun getAllStories(source: Source): List<StoryModel>
 }
 
-class StoryRepositoryImpl: StoryRepository {
-    private val db = Firebase.firestore
-    private val columnName = "student-stories"
 
-    override suspend fun getAllStories(): List<StoryModel> {
+object StoryRepositoryImpl: StoryRepository {
+    private const val COLLECTION_NAME = "student-stories"
+
+    override suspend fun getAllStories(source: Source): List<StoryModel> {
+        Log.d("FIRESTORE", "Fetching from Source.${source.name}")
         try {
-            val result = db.collection(columnName).get().await()
+            val result = Firebase.firestore
+                .collection(COLLECTION_NAME)
+                .get(source)
+                .await()
 
             return result.map { document ->
                 document.toObject(StoryModel::class.java)
