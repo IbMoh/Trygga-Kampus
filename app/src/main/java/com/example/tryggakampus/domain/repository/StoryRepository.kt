68eded7deb22fcbb528486bfc2beaa6d1418a337
idontreallywolf <nedim.kanat.lp@gmail.com>
2024-10-28 15:19:47 +0100

    refactor error handling for StoryRepository

--- app/src/main/java/com/example/tryggakampus/domain/repository/StoryRepository.kt
+++ app/src/main/java/com/example/tryggakampus/domain/repository/StoryRepository.kt
@@ -2,6 +2,7 @@ package com.example.tryggakampus.domain.repository
 
 import android.util.Log
 import com.example.tryggakampus.domain.model.StoryModel
+import com.google.firebase.FirebaseException
 import com.google.firebase.firestore.QuerySnapshot
 
 import com.google.firebase.firestore.Source
@@ -20,7 +21,7 @@ interface StoryRepository {
         isAnonymous: Boolean? = true
     ): StoryModel?
 
-    suspend fun fetchAll(source: Source): QuerySnapshot
+    suspend fun fetchAll(source: Source): QuerySnapshot?
 }
 
 object StoryRepositoryImpl: StoryRepository {
@@ -29,15 +30,15 @@ object StoryRepositoryImpl: StoryRepository {
     override suspend fun getAllStories(source: Source): List<StoryModel> {
         val result = this.fetchAll(source)
 
-        return result.map { document ->
+        return result?.map { document ->
             val doc = document.toObject(StoryModel::class.java).apply {
                 id = document.id
             }
             doc
-        }
+        } ?: emptyList()
     }
 
-    override suspend fun fetchAll(source: Source): QuerySnapshot {
+    override suspend fun fetchAll(source: Source): QuerySnapshot? {
         var result: QuerySnapshot
         val ref = Firebase.firestore.collection(COLLECTION_NAME)
 
@@ -46,9 +47,24 @@ object StoryRepositoryImpl: StoryRepository {
                 Log.d("StoryRepository::fetchAll", "Trying to fetch from SERVER")
             }
             result = ref.get(source).await()
-        } catch (e: Exception) {
+        }
+
+        /*
+            This is to catch a general firebase exception,
+            there are various other sub-exceptions that
+            can be handled separately, but for now we'll
+            leave it at this.
+        */
+        catch (e: FirebaseException) {
+            Log.d("FATAL", e.stackTraceToString())
             Log.d("StoryRepository::fetchAll", "Trying to fetch from CACHE, Server connection failed.")
             result = ref.get(Source.CACHE).await()
+            return result
+        }
+
+        catch (e: Exception) {
+            Log.d("FATAL", e.message ?: e.stackTraceToString())
+            return null
         }
 
         return result
