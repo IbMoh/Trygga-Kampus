ce4879dfa0cff9a989c4aee38d04b54671a4f91a
idontreallywolf <nedim.kanat.lp@gmail.com>
2024-11-06 18:27:09 +0100

    refactor & redesign

--- app/src/main/java/com/example/tryggakampus/presentation/formPage/FormPage.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/formPage/FormPage.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -14,10 +15,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tryggakampus.R
+import com.example.tryggakampus.presentation.component.OutlinedInput
 import com.example.tryggakampus.presentation.component.PageContainer
 
 @Composable
@@ -39,54 +42,66 @@ fun FormPage(title: String) {
                 painter = painterResource(R.drawable.helping_hands1),
                 contentDescription = "helping hands logo"
             )
-            FormsFields(
-                name,
-                subject,
-                message
-            )
-            SubmitButton(
-                name.value.text,
-                subject.value.text,
-                message.value.text
-            )
+            Column (modifier = Modifier
+                .clip(RoundedCornerShape(10.dp))
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(15.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Get in touch", fontSize = 20.sp, fontWeight = FontWeight.SemiBold)
+                Text("receive the help you need", fontSize = 14.sp) // not sure, Per might want a different text here
+                FormsFields(
+                    name.value.text,
+                    onChangeName = { name.value = TextFieldValue(it) },
+                    subject.value.text,
+                    onChangeSubject = { subject.value = TextFieldValue(it) },
+                    message.value.text,
+                    onChangeMessage = { message.value = TextFieldValue(it) }
+                )
+                SubmitButton(
+                    name.value.text,
+                    subject.value.text,
+                    message.value.text
+                )
+            }
         }
     }
 }
 
 @Composable
 fun FormsFields(
-    name: MutableState<TextFieldValue>,subject: MutableState<TextFieldValue>,message: MutableState<TextFieldValue>
+    name: String,
+    onChangeName: (s: String) -> Unit,
+
+    subject: String,
+    onChangeSubject: (s: String) -> Unit,
+
+    message: String,
+    onChangeMessage: (s: String) -> Unit
 ){
-    TextField(
-        value = name.value,
-        onValueChange = { name.value = it },
-        placeholder = { Text(text = "Enter Your Name Here") },
-        modifier = Modifier.fillMaxWidth()
-            .clip(shape = RoundedCornerShape(10.dp)),
+    OutlinedInput(
+        value = name,
+        onValueChange = onChangeName,
+        label = "Enter your name",
         singleLine = true
     )
 
     Spacer(modifier = Modifier.height(10.dp))
 
-    TextField(
-        value = subject.value,
-        onValueChange = { subject.value = it },
-        placeholder = { Text(text = "Subject Field") },
-        modifier = Modifier.fillMaxWidth()
-            .clip(shape = RoundedCornerShape(10.dp))
+    OutlinedInput(
+        value = subject,
+        onValueChange = onChangeSubject,
+        label = "Subject Field",
+        singleLine = true
     )
 
     Spacer(modifier = Modifier.height(10.dp))
 
-    TextField(
-        value = message.value,
-        onValueChange = { message.value = it },
-        placeholder = { Text(text = "Enter Your Message Here") },
-        modifier = Modifier
-            .clip(shape = RoundedCornerShape(10.dp))
-            .fillMaxWidth()
-            .height(200.dp),
-        singleLine = false
+    OutlinedInput(
+        value = message,
+        onValueChange = onChangeMessage,
+        label = "Enter your message here"
     )
 
     Spacer(modifier = Modifier.height(20.dp))
@@ -120,12 +135,12 @@ fun SubmitButton(
             }
         },
         modifier = Modifier
-            .clip(shape = RoundedCornerShape(40.dp))
+            .clip(shape = CircleShape)
             .size(width = 200.dp, height = 60.dp)
             .background(Color.Red),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Color.Red,
-            contentColor = Color.White
+            containerColor = MaterialTheme.colorScheme.error,
+            contentColor = MaterialTheme.colorScheme.onErrorContainer
         )
     ) {
         Text(
